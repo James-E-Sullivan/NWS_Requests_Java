1332db8a4c6dd67829ff71abee8d80293b65beff
james.sullivan <jesullivan222@gmail.com>
2020-06-03 11:15:57 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        logger.info("Testing Log4j");
+    }
+
+
+}
